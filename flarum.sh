050ee7cbbaaefe80d892ca8cb779228126f4ca3d
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-02-21 22:05:53 -0500

    (slightly) improved installation script

--- flarum.sh
+++ flarum.sh
@@ -1,7 +1,57 @@
-mkdir .tmp
-cd .tmp
-curl -o flarum.zip --remote-name --location https://github.com/askvortsov1/flarum-one-click/releases/latest/download/release-bazaar.zip
-unzip flarum.zip -d flarum
-cd ..
-mv .tmp/flarum/* .
-rm -rf .tmp
+#!/bin/bash
+
+OVERRIDE_PATHS=("site.php" "index.php" "flarum" "assets" "storage" ".htaccess" ".nginx.conf")
+NON_OVERRIDE_PATHS=("config.php" "composer.json" "extend.php")
+
+interactive=1
+
+
+mk_tmp_dir()
+{
+    mkdir .tmp
+}
+
+rm_tmp_dir()
+{
+    rm -rf .tmp
+}
+
+download()
+{
+    curl -o .tmp/flarum.zip --remote-name --location https://github.com/askvortsov1/flarum-one-click/releases/latest/download/release-bazaar.zip
+    unzip .tmp/flarum.zip -d .tmp/flarum
+}
+
+compat()
+{
+    if [ -f "public/assets" ]; then
+        mv public/assets assets
+    fi
+    if [ -f "bootstrap.php" ]; then
+        mv bootstrap.php extend.php
+    fi
+    rm -rf public
+}
+
+install()
+{
+    for t in ${OVERRIDE_PATHS[@]}; do
+        mv ".tmp/flarum/$t" .
+    done
+    for t in ${NON_OVERRIDE_PATHS[@]}; do
+        if [ ! -f "$t" ]; then
+            mv ".tmp/flarum/$t" .
+        fi
+    done
+}
+
+
+# MAIN
+
+mk_tmp_dir
+download
+compat
+install
+rm_tmp_dir
+
+
